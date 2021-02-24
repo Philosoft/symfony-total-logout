e9b1fd1cf55f40c63e44cb19419785f72c548b04
Alexander Frolov <thephilosoft@gmail.com>
2021-02-23 19:09:56 -0500

    feat(security): add default user (admin) via fixtures (+ migration)
    https://symfony.com/doc/current/security.html#c-encoding-passwords

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setPassword($this->passwordEncoder->encodePassword($admin, '12345'));
+        $manager->persist($admin);
 
         $manager->flush();
     }
